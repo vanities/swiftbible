41af92c9e6ef1dbb77117375d8bfad1ec48b4e1d
Adam Mischke <mischkeaa@gmail.com>
2024-09-16 13:19:22 -0500

    only use certain versions for the devo

--- supabase/functions/daily-devotional/index.ts
+++ supabase/functions/daily-devotional/index.ts
@@ -16,7 +16,7 @@ Deno.serve(async (req) => {
     baseURL: "https://api.lambdalabs.com/v1",
     apiKey: Deno.env.get("LAMBDA_API_KEY"),
   });
-  const query = `Create a daily devotional for a Bible app based on a random Bible verse. Incorporate todays date: ${new Date()} into it, if it makes sense to. You can use markdown syntax to format the text.`;
+  const query = `Create a daily devotional for a Bible app based on a random Bible verse, don't quote the NIV, only quote the KJV, NKJV, or the ESV.. Incorporate todays date: ${new Date()} into it, if it makes sense to. You can use markdown syntax to format the text.`;
   const chatCompletion = await openai.chat.completions.create({
     messages: [{ role: "user", content: query }],
     model: "hermes-3-llama-3.1-405b-fp8",
