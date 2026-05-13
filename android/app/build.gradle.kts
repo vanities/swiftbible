e370f6fb7892bed7c093b919276a9a72c4bedaa2
Adam Mischke <mischkeaa@gmail.com>
2026-05-13 16:30:40 -0500

    Bump to 1.43 (iOS build 7, Android build 8)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "biz.am2.swiftbible"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.42"
+        versionCode = 8
+        versionName = "1.43"
         vectorDrawables { useSupportLibrary = true }
     }
 
